5619392401e4334091f30678f003f0fc85276383
Keith Dunwell <keithdunwel@gmail.com>
2022-08-04 19:08:30 -0500

    Desafío APIRESTful completado

---
+++ 4-APIRESTful/app.js
@@ -0,0 +1,141 @@
+const express = require("express")
+const app = express()
+
+const PORT = process.env.PORT || 8080
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static("public"))
+
+//////////////////////////////////////////////////////
+///////////////////////////////////////////////////////
+
+const { Router } = express
+const router = Router()
+const fs = require("fs")
+
+router.get("/productos", (req, res) => {
+    fs.readFile("productos.txt", "utf-8", (err, products) => {
+        
+        if (err) {
+            res.send("No puedo, toy chikito :(")
+        }else{
+            res.send(JSON.parse(products))
+        }
+    })
+})
+
+router.post("/productos", (req, res) => {
+    const {title, price, thumbnail} = req.body
+
+    fs.readFile("productos.txt", "utf-8", (err, products) => {
+        
+        if (err) {
+            res.send("No puedo, toy chikito :(")
+        }else{
+			const dataParsed = JSON.parse(products)
+            const product = {"title": title, "price": parseInt(price), "thumbnail": thumbnail, "id": dataParsed.length + 1}
+			dataParsed.push(product)
+
+			const newText = JSON.stringify(dataParsed)
+			fs.writeFileSync("productos.txt", newText)
+
+            res.json({success: true, newProduct: product})
+        }
+    })
+})
+
+router.get("/productos/:id", (req, res) => {
+    const { id } = req.params
+
+    fs.readFile("productos.txt", "utf-8", (err, products) => {
+        
+        if (err) {
+            res.send("No puedo, toy chikito :(")
+        }else{
+			const dataParsed = JSON.parse(products)
+			const getProduct = dataParsed.findIndex((product => product["id"] === parseInt(id)))
+			res.send(dataParsed[getProduct] || "El ID ingresado no existe.")
+        }
+    })
+})
+
+router.put("/productos/:id", (req, res) => {
+    const { id } = req.params
+    const {title, price, thumbnail} = req.body
+
+    fs.readFile("productos.txt", "utf-8", (err, products) => {
+        
+        if (err) {
+            res.send("No puedo, toy chikito :(")
+        }else{
+            const dataParsed = JSON.parse(products)
+			const getProduct = dataParsed.findIndex((product => product["id"] === parseInt(id)))
+
+			if (getProduct > -1) {
+				
+                const product = dataParsed[getProduct]
+
+                if (title) {
+                    product ["title"] = title
+                }
+
+                if (price) {
+                    product ["price"] = price
+                }
+
+                if (thumbnail) {
+                    product ["thumbnail"] = thumbnail
+                }
+
+                dataParsed.push(product)
+				const newText = JSON.stringify(dataParsed)
+				fs.writeFileSync("productos.txt", newText)
+
+				res.send("Producto actualizado")
+
+			}else{
+				res.send("El ID ingresado no existe.")
+			}
+        }
+    })
+    
+})
+
+router.delete("/productos/:id", (req, res) => {
+    const { id } = req.params
+
+    fs.readFile("productos.txt", "utf-8", (err, products) => {
+        
+        if (err) {
+            res.send("No puedo, toy chikito :(")
+        }else{
+            const dataParsed = JSON.parse(products)
+			const getProduct = dataParsed.findIndex((product => product["id"] === parseInt(id)))
+
+			if (getProduct > -1) {
+				dataParsed.splice(getProduct, 1)
+
+				const newText = JSON.stringify(dataParsed)
+				fs.writeFileSync("productos.txt", newText)
+
+				res.send("Producto eliminado")
+
+			}else{
+				res.send("El ID ingresado no existe.")
+			}
+        }
+    })
+})
+
+
+//////////////////////////////////////////////////////
+//////////////////////////////////////////////////////
+
+app.use("/api", router)
+
+
+app.listen(PORT, err => {
+    if (err) throw err
+    console.log("Server running on port 8080")
+})
---
+++ 4-APIRESTful/contenedor.js
@@ -0,0 +1,91 @@
+const fs = require("fs")
+
+class Contenedor {
+    constructor(file){
+		this.file = file;
+    }
+
+	//save(Object): Number - Recibe un objeto, lo guarda en el archivo, devuelve el id asignado.
+	async save(product) {
+		
+		try{
+			const data = await fs.readFileSync(this.file, "utf-8")
+			const dataParsed = JSON.parse(data)
+
+			product["id"] = dataParsed.length + 1
+			dataParsed.push(product)
+
+			const newText = JSON.stringify(dataParsed)
+			fs.writeFileSync(this.file, newText)
+
+		}catch (err) {
+			console.error(err)
+		}
+
+	}
+
+	//getById(Number): Object - Recibe un id y devuelve el objeto con ese id, o null si no está.	
+	async getById(id) {
+
+		try{
+			const data = await fs.readFileSync(this.file, "utf-8")
+			const dataParsed = JSON.parse(data)
+			const getProduct = dataParsed.findIndex((product => product["id"] === id))
+
+			console.log(dataParsed[getProduct] || "El ID ingresado no existe.")
+
+		}catch (err) {
+			console.error(err)
+		}
+
+
+	}
+
+	//getAll(): Object[] - Devuelve un array con los objetos presentes en el archivo.
+	async getAll() {
+		try{
+			const data = await fs.readFileSync(this.file, "utf-8")
+			const dataParsed = JSON.parse(data)
+
+			return dataParsed
+
+		}catch (err) {
+			console.error(err)
+			return "No puedo, toy chikito :("
+		}
+	}
+
+	//deleteById(Number): void - Elimina del archivo el objeto con el id buscado.
+ 	async deleteById(id) {
+
+		try{
+			const data = await fs.readFileSync(this.file, "utf-8")
+			const dataParsed = JSON.parse(data)
+			const getProduct = dataParsed.findIndex((product => product["id"] === id))
+
+			if (getProduct > -1) {
+				dataParsed.splice(getProduct, 1)
+
+				const newText = JSON.stringify(dataParsed)
+				fs.writeFileSync(this.file, newText)
+
+				console.log("Producto eliminado")
+
+			}else{
+				console.log("El ID ingresado no existe.")
+			}
+
+		}catch (err) {
+			console.error(err)
+		}
+
+	}
+	
+	//deleteAll(): void - Elimina todos los objetos presentes en el archivo.
+	async deleteAll() {
+		fs.writeFileSync(this.file, "[]")
+	}
+
+}
+
+module.exports = Contenedor
