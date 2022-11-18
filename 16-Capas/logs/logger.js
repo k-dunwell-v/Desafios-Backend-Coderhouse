09f619a5069e4d057c37990bb9cf6a9d5c4ec469
Keith Dunwell <keithdunwel@gmail.com>
2022-11-17 21:24:11 -0500

    Corrección logs

--- 16-Capas/logs/logger.js
+++ 16-Capas/logs/logger.js
@@ -3,9 +3,9 @@ const winston = require('winston')
 const logger = winston.createLogger({
     transports : [
         new winston.transports.Console({ level:'verbose' }),
-        new winston.transports.File({ filename: '../logs/info.log', level:'info' }),
-        new winston.transports.File({ filename: '../logs/warn.log', level:'warn' }),
-        new winston.transports.File({ filename: '../logs/error.log', level:'error' }),
+        new winston.transports.File({ filename: './logs/info.log', level:'info' }),
+        new winston.transports.File({ filename: './logs/warn.log', level:'warn' }),
+        new winston.transports.File({ filename: './logs/error.log', level:'error' }),
     ]
 })
 
